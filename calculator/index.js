961cb17dfdbfc18c4ae2a721e447f3e12a81bf83
Artur <a.sincarenco.97@gmail.com>
2022-05-08 13:24:25 +0300

    feat: add calculator class

--- calculator/index.js
+++ calculator/index.js
@@ -1,7 +1,41 @@
-const numBtn = document.querySelectorAll('[data-number]')
-const opBtn = document.querySelectorAll('[data-operation]')
-const equalsBtn = document.querySelector('[data-equals]')
-const deleteBtn = document.querySelector('[data-delete]')
-const allClearBtn = document.querySelector('[data-all-clear]')
-const previousOperandEl = document.querySelector('[data-previous-operand]')
-const currentOperandEl = document.querySelector('[data-current-operand]')
+class Calculator {
+    constructor(previousOperandEl, currentOperandEl) {
+        this.previousOperandEl = previousOperandEl
+        this.currentOperandEl = currentOperandEl
+    }
+
+    clear() {
+        this.currentOperandEl = ""
+        this.previousOperandEl = ""
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNum(number) {
+
+    }
+
+    chooseOp(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    update() {
+
+    }
+}
+
+const numBtn = document.querySelectorAll("[data-number]")
+const opBtn = document.querySelectorAll("[data-operation]")
+const equalsBtn = document.querySelector("[data-equals]")
+const deleteBtn = document.querySelector("[data-delete]")
+const allClearBtn = document.querySelector("[data-all-clear]")
+const previousOperandEl = document.querySelector("[data-previous-operand]")
+const currentOperandEl = document.querySelector("[data-current-operand]")
+
